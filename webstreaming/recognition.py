ce0c954227435c49bd0409cda95a1c103be1a07a
jjPenguin <43468958+jjPenguin@users.noreply.github.com>
2020-09-02 12:52:15 +0900

    Update recognition.py
    수정

--- webstreaming/recognition.py
+++ webstreaming/recognition.py
@@ -12,23 +12,24 @@ datalist=[]
 ymlList=[]
 descs = {}
 model = cv2.face.LBPHFaceRecognizer_create()
-data_path = 'trainer/'
+data_path = 'trainer/personal/'
 with open('img_name_match.pickle', 'rb') as fr:
+    nameList = []
     while True:
         try:
             data = pickle.load(fr)
         except EOFError:
-            break
-        datalist.append(data)
+            break        
+        for i in data:
+            nameList.append(i)
 
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 print(onlyfiles)
 
 for i in onlyfiles:
-    only = os.path.splitext(i)[-1]
-    if only == '.yml':
-        model.read(data_path+i)
-        print(data_path+i)
+    print(i)
+    model.read(data_path+i)
+        
 
 face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 ds_factor=0.6
@@ -40,6 +41,7 @@ class FaceRecognition:
         self.found = []
         self.count1 = 0 
         self.count2 = 0
+        self.nameList = []
         self.face_result = 0
 
     def __del__(self):
@@ -53,24 +55,34 @@ class FaceRecognition:
                     data = pickle.load(fr)
                 except EOFError:
                     break
-                datalist.append(data)
-        return datalist
+                for i in data:
+                    nameList.append(i)
+        return nameList
 
     def face_found(self, face):
-        
-        self.found.append(face)
-        if len(self.found) == 10:    
-            for i in self.found:
-                if i == 1:
-                    self.count1 = self.count1 + 1
+        if face != 0:
+            self.found.append(face)
+            if len(self.found) < 20:
+                for i in self.found:
+                    if i == 1:
+                        self.count1 = self.count1 + 1
+                    else :
+                        self.count2 = self.count2 + 1 
+                
+                if self.count1 >= self.count2 :
+                    self.face_result = 555
+                    return self.face_result
                 else :
-                    self.count2 = self.count2 + 1
-            if self.count1 >= self.count2 :
-                self.face_result = 555
-                return 555
+                    self.face_result = 333
+                    return self.face_result
             else :
-                self.face_result = 333
-                return 333
+                pass
+        elif face == 0:
+            self.found.append(face)
+            if len(self.found) == 10:
+                self.face_result = 111
+                return self.face_result
+        
 
     def face_detector(self, frame):
         gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -89,18 +101,20 @@ class FaceRecognition:
         # get video data from camera 
         ret, frame = self.video.read()
         frame=cv2.flip(frame, 1)
+        nameDataList = []
         # try face detect
         frame,face = self.face_detector(frame)
-        
+        nameDataList = self.get_pickle('img_name_match.pickle')
+        facefound = 0
+
         try:
             #detected pic transform to grayscale
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
             #training model try predict
             id, result = model.predict(face)
-            #result[1] is confidence and close to 0
             #this mean registered user
-            #print(datalist[result[0]])
-            
+            label = nameDataList[id]
+
             if result < 500:
                 confidence = int(100*(1-(result)/300))
                 # display confidence
@@ -111,27 +125,23 @@ class FaceRecognition:
             #over 75 same persone return UnLocked! 
             if confidence > 75:
                 facefound = 1
-                test = self.face_found(facefound)
-                cv2.putText(frame, " Unlocked", (0, 700), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                #test = self.face_found(facefound)
+                cv2.putText(frame, label + " Unlocked", (0, 700), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
                 ret, jpeg = cv2.imencode('.jpg', frame)
-                
-                return jpeg.tobytes()
+                return jpeg.tobytes(), facefound
                 
             else:
                 #under 75 other person return Locked!!! 
-                facefound = 0
-                test = self.face_found(facefound)
+                facefound = 2
+                #test = self.face_found(facefound)
                 cv2.putText(frame, "Locked", (0, 700), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
                 ret, jpeg = cv2.imencode('.jpg', frame)
-                    
-                return jpeg.tobytes()
+                return jpeg.tobytes(), facefound
                 
         except:
             #not face recognition 
+            facefound = 0
             cv2.putText(frame, "Face Not Found", (0, 700), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 10, 10), 2)
             ret, jpeg = cv2.imencode('.jpg', frame)
-            return jpeg.tobytes()
+            return jpeg.tobytes(), facefound
             pass
-
-
-
