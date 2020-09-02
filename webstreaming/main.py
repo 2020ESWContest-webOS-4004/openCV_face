b17e72900fb0d408c2bcff2171853ffccc9f8e2d
jjPenguin <43468958+jjPenguin@users.noreply.github.com>
2020-09-02 12:52:37 +0900

    Update main.py
    수정

--- webstreaming/main.py
+++ webstreaming/main.py
@@ -1,37 +1,48 @@
-from flask import Flask, render_template, Response
-from flask_socketio import SocketIO
+from flask import Flask, render_template, Response, request
+from flask_socketio import SocketIO, emit, send
 # from camera import VideoCamera
 from recognition import FaceRecognition
+import recognition
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'BCODE_Flask'
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+host = '0.0.0.0'
+port = '5000'
 
 @app.route('/')
 def index():
     # rendering webpage
     return render_template('index.html')
 
+def face_result(result):
+
+    if result == 555:
+        return print('Recognition')
+    elif result == 333:
+        return print('Not Recognition')
+    else :
+        return print('Not found face')
+
+
 def gen(recognition):
     while True:
         #get camera frame
-        frame = recognition.get_frame()
+        frame, f = recognition.get_frame()
+        found = recognition.face_found(f)
         
         yield (b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-def gen_face_result(recognition):
-    re = recognition.face_result
-    print(re)
-
+    
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(FaceRecognition()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@socketio.on('my event')
-def test_message(message):
-    emit('my response', {'data' : 'got it!'})
-    
+@app.route('/')
+def video_result():
+    return Response(gen_face(FaceRecognition()))
+
+
 if __name__ == '__main__':
     # defining server ip address and port
-    socketio.run(app,host='0.0.0.0',port='5000')
+    socketio.run(app,host,port)
