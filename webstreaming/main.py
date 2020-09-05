f03016794e19361a3e569e6c9133a372a10eb40d
jjPenguin <43468958+jjPenguin@users.noreply.github.com>
2020-09-06 03:44:48 +0900

    Update main.py
    라즈베리파이 카메라 작동 코드 추가

--- webstreaming/main.py
+++ webstreaming/main.py
@@ -6,39 +6,49 @@
 #*  description     :   플라스크 서버
 #**
 
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, emit, send
 # from camera import VideoCamera
 from recognition import FaceRecognition
-import recognition
+import json
+import os
+
+#newCode="sudo modprobe bcm2835-v4l2"
+#os.system(newCode)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 host = '0.0.0.0'
 port = '5000'
+found = []
 
 @app.route('/')
 def index():
     # rendering webpage
     return render_template('index.html')
 
-def face_result(result):
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+def face_result(result):
     if result == 555:
         return print('Recognition')
     elif result == 333:
         return print('Not Recognition')
-    else :
-        return print('Not found face')
-
 
 def gen(recognition):
     while True:
         #get camera frame
         frame, f = recognition.get_frame()
         found = recognition.face_found(f)
-        
+        #send_data(face_result(found))
         yield (b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
     
 
