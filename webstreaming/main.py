094ba2115d99ec957a07f28cb2b737ceb0d26a13
jjPenguin <43468958+jjPenguin@users.noreply.github.com>
2020-10-03 02:14:12 +0900

    Update main.py
    불필요한 코드 삭제 및 데이터 베이스 사용자 정보 조회 코드 및 주석 추가

--- webstreaming/main.py
+++ webstreaming/main.py
@@ -1,14 +1,15 @@
 #**
 #*  service         :   main.py
 #*  type            :   python3
-#*  date            :   2020.09.14
+#*  date            :   2020.10.03
 #*  author          :   한지훈(RORA)
 #*  description     :   플라스크 서버
 #**
 
+# -*- coding: utf-8 -*- #
 from flask import Flask, render_template, Response,request
 from flask_socketio import SocketIO, emit, send
-# from camera import VideoCamera
+import socketio as SocketIO_client
 from recognition import FaceRecognition
 import json
 import os
@@ -18,22 +19,24 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socket_server = SocketIO(app)
+socket_client = SocketIO_client.Client()
+
 host = '0.0.0.0'
 port = '5000'
 count = 0 
 face_id = 0 
-face_name = ''
+jarvis_mi = ' '
+
+# 클라이언트 연결
+#socket_client.connect('http://192.168.1.8:5555')
 
 @app.route('/')
 def index():
-    # rendering webpage
+    # 웹페이지 렌더링
     return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-def get_json(l):
+""" def get_json(l):
     global face_id
     with open('name.json', 'r') as json_file:
         json_data = json.load(json_file)
@@ -46,31 +49,56 @@ def get_json(l):
                     face_id = j['id']
             except IndexError:
                 break
-    return face_id
+    return face_id """
+
+#DB 데이터 접속 및 사용자 조회
+def sqlResult(name):
+    import pymysql
+    conn = pymysql.connect(host='', user='', password='', db='' ) #DB 연결
+    cur = conn.cursor()
+    sql = "select member_name from jarvis_member where jarvis_id ='{}'".format(name)
+    cur.execute(sql)
+    row = cur.fetchone()
+    while row:
+        name = row[0]
+        row = cur.fetchone()
+
+    if name is not None:
+        conn.close()
+        return name
+    else :
+        return False
 
 def gen(recognition):
-    global face_name, count, face_id
+    global jarvis_mi, count, face_id
+    i = 0
     while True:
+        i = i+1
+        frame = recognition.get_test()
+        # 실시간 영상 송출
+        yield (b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        if i == 30:
+            break 
+    
+    while True:  
         frame, label = recognition.get_frame()
-        count = count + 1
-        if count == 10:
-            face_name = label
-            break
-        face_id = get_json(face_name)
-        
+        jarvis_mi = label
+        # 실시간 영상 송출
         yield (b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n' )
-
+        # 리턴 받은 label을 이용하여 DB조회, 사용자 인증
+        sr = sqlResult(jarvis_mi)
+        
+        # 사용자 인증시 결과 전송
+        if (sr != "") and (sr != " "):
+            result = json.dumps({'result': True, 'userid': jarvis_mi, 'username': sr })
+            socket_client.emit('auth-data', result)
+            """ recognition.__del__()
+            exit() """
+            
 @app.route('/video_feed')
-def video_feed():
+def video_feed(): 
     return Response(gen(FaceRecognition()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@socketio.on('my event')
-def handle_my_custom_event(test):
-    global face_name, face_id
-    print('received my event: ' + str(test))
-    #print(json.dumps({face_id : face_name}))
-    socketio.emit('my response', json.dumps({face_id : face_name}) )
-   
 if __name__ == '__main__':
-    # defining server ip address and port
-    socketio.run(app,host,port)
+    # 서버 ip주소와 포트 
+    socket_server.run(app,host,port)
